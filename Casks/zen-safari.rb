30374931854cca90742fab521486cd25c252ea7c
caius72 <tuschner@gmail.com>
2026-09-22 20:35:39 +0200

    zen-safari 1.0.0

--- Casks/zen-safari.rb
+++ Casks/zen-safari.rb
@@ -1,6 +1,6 @@
 cask "zen-safari" do
-  version "0.0.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  version "1.0.0"
+  sha256 "0cea9d5f821e646e47444ae0acef66425a1f8b795e4d909fb7e58e824329dd0a"
 
   url "https://github.com/caius72/zen/releases/download/v#{version}/Zen-#{version}.zip"
   name "Zen"
